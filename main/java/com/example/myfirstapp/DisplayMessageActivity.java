42a413a04d05f8b1f56319e98728f55c79b3b5f1
SilvT1 <42608238+SilvT1@users.noreply.github.com>
2019-05-30 13:49:28 +0100

    Adding files for First Mobile app (Self Learning)

---
+++ main/java/com/example/myfirstapp/DisplayMessageActivity.java
@@ -0,0 +1,40 @@
+package com.example.myfirstapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
+import android.widget.Toast;
+
+public class DisplayMessageActivity extends AppCompatActivity {
+    private int mCount = 0;
+    private TextView mShowCount;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display_message);
+        mShowCount = (TextView) findViewById(R.id.show_count);
+
+        // Get the Intent that started this activity and extract the string
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+
+        // Capture the layout's TextView and set the string as its text
+        TextView textView = findViewById(R.id.textView);
+        textView.setText(message);
+    }
+
+    public void showCount(View view) {
+        Toast count = Toast.makeText(this, R.string.count_message,
+                Toast.LENGTH_SHORT);
+        count.show();
+    }
+
+    public void countUp(View view) {
+        ++mCount;
+        if (mShowCount != null)
+            mShowCount.setText(Integer.toString(mCount));
+    }
+}
---
+++ main/java/com/example/myfirstapp/MainActivity.java
@@ -0,0 +1,41 @@
+package com.example.myfirstapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView greeting;
+    Button click;
+    public static final String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        greeting = findViewById(R.id.tvGreeting);
+        click = findViewById(R.id.btnClick);
+        click.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                greeting.setVisibility(View.VISIBLE);
+            }
+        });
+    }
+    /** Called when the user taps the Send button */
+    public void sendMessage(View view) {
+        // Do something in response to button
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = (EditText) findViewById(R.id.editText);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+
+    }
+}
+
